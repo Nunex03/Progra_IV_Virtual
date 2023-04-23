3b287201ea33d9091ee438b3b96377ca4c33f6b5
Alejandro Núñez <josh.nunex@gmail.com>
2023-04-23 15:25:47 -0600

    lógica

--- index.php
+++ index.php
@@ -7,7 +7,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>ISNP998919</title>
 </head>
-<body class="row align-items-center">
+<body>
+    <div class="position-absolute start-50">
     <form action="procesar.php" method="POST">
         <label for="">Nombre del Artículo</label><br>
         <input type="text" name="txtNombre" id="txtNombre" placeholder="Ej. PC Gamer" required><br><br>
@@ -22,5 +23,7 @@
         <input type="submit" value="Calcular Impuestos" class="btn btn-success">
 
     </form>
+    </div>
+    
 </body>
 </html>
--- procesar.php
+++ procesar.php
@@ -1,6 +1,15 @@
 <?php
-    $nombreProducto = '';
-    $precioProducto = '';
-    $cantidad = '';
+    $nombreProducto = $_POST['txtNombre'];
+    $precioProducto = $_POST['txtPrecioU'];
+    $cantidad = $_POST['txtCantidad'];
 
+    $Subtotal = ($precioProducto * $cantidad);
+    $IVA = (($precioProducto * $cantidad) * 0.13);
+    $CESC = (($precioProducto * $cantidad) *0.05);
+    $TotalaPagar = $Subtotal + $IVA + $CESC;
+
+   echo "Subtotal: $" . $Subtotal . "<br>" .
+        "IVA: $" . $IVA . "<br>" .
+        "CESC: $" . $CESC . "<br>" .
+        "Total a Pagar: $" . $TotalaPagar . "<br>";
 ?>

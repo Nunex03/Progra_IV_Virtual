e644b3c2e42fb237e8ec4790f774a55800df1e33
Alejandro Núñez <josh.nunex@gmail.com>
2023-04-23 14:36:55 -0600

    maquetación y un poquito de diseño

--- index.php
+++ index.php
@@ -4,9 +4,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>ISNP998919</title>
 </head>
-<body>
-    
+<body class="row align-items-center">
+    <form action="procesar.php" method="POST">
+        <label for="">Nombre del Artículo</label><br>
+        <input type="text" name="txtNombre" id="txtNombre" placeholder="Ej. PC Gamer" required><br><br>
+
+        <label for="">Precio Unitario</label><br>
+        <input type="text" name="txtPrecioU" id="txtPrecioU" required><br><br>
+
+        <label for="">Cantidad</label><br>
+        <input type="text" name="txtCantidad" id="txtCantidad" required><br><br>
+
+
+        <input type="submit" value="Calcular Impuestos" class="btn btn-success">
+
+    </form>
 </body>
 </html>
---
+++ procesar.php
@@ -0,0 +1,6 @@
+<?php
+    $nombreProducto = '';
+    $precioProducto = '';
+    $cantidad = '';
+
+?>
